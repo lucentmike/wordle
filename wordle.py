6265ab5d11718edf8bfbca2bbcf875c10349dfe8
Mike Lucent <mike.j.lucent@gmail.com>
2022-04-11 23:34:10 -0700

    finished code, need to do word check against list

--- wordle.py
+++ wordle.py
@@ -1,19 +1,127 @@
+from operator import le
+from pickle import NONE
 import random, pygame, sys
+from xml.etree.ElementTree import TreeBuilder
 pygame.init() 
 
 WHITE = (255,255,255)
 YELLOW = (255,255,102)
-GREY = (211,211,211)
+GREY = (190,190,190)
 BLACK = (0,0,0)
 GREEN = (0,255,0)
-LIGHTGREEN = (153,255,204)
+LIGHTGREEN = (125,238,125)
 
 font = pygame.font.SysFont('Helvetica neue', 40)
-bigFont = pygame.font.SysFont('Helvectia neue', 80)
+bigFont = pygame.font.SysFont('Helvectia neue', 70)
 
-youWin = bigFont.render('You Win!', True, LIGHTGREEN)
-youLose = bigFont.render('You Lose!', True, LIGHTGREEN)
+youWin = bigFont.render('NICE!', True, LIGHTGREEN)
+youLose = bigFont.render(f'DAMN!', True, LIGHTGREEN)
 playAgain = bigFont.render('Play Again?', True, LIGHTGREEN)
+notValid = bigFont.render('Not a valid word!', True, LIGHTGREEN)
 
 def checkGuest(turns, word, userGuess, window):
-    pass
+    renderList = ["","","","",""]
+    spacing = 0
+    guessColorCode = [GREY, GREY, GREY, GREY, GREY]
+
+    for letter in range(0,5):
+        if userGuess[letter] in word:
+            guessColorCode[letter] = YELLOW
+
+        if userGuess[letter] == word[letter]:
+            guessColorCode[letter] = GREEN
+
+        print(userGuess[letter])
+        print(guessColorCode[letter])
+
+    list(userGuess)
+
+    for letter in range(0,5):
+        renderList[letter] = font.render(userGuess[letter], True, BLACK)
+        pygame.draw.rect(window, guessColorCode[letter], pygame.Rect(60 + spacing, 50 + (turns*80), 50, 50))
+        window.blit(renderList[letter], (70 + spacing, 50 + (turns*80)))
+        spacing +=80
+
+    if guessColorCode == [GREEN, GREEN, GREEN, GREEN, GREEN]:
+        return True
+    print(guessColorCode)
+
+    
+def main():
+    with open('words.txt', 'r') as f:
+        WORDS = f.readlines()
+
+    with open('guess_words.txt', 'r') as f:
+        GUESS_WORDS = f.readlines()
+
+    word = WORDS[random.randint(0, len(WORDS)-1)].upper().strip()
+    print(word)
+
+    FPS = 30
+    clock = pygame.time.Clock()
+
+    HEIGHT = 600
+    WIDTH = 500
+
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    window.fill(BLACK)
+
+    turns = 0
+    guess = ''
+
+    for x in range(0,5):
+        for y in range(0,6):
+            pygame.draw.rect(window, GREY, pygame.Rect(60 + (x*80), 50 + (y*80), 50, 50), 2)
+
+    pygame.display.set_caption("Wordl")
+
+    win = False
+
+    while True:
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                pygame.exit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE or len(guess) > 5:
+                    guess = guess[:-1]
+
+            elif event.type == pygame.KEYDOWN and event.key != pygame.K_BACKSPACE:
+                guess += event.unicode.upper().strip()
+                print(type(guess))
+                print(len(guess))
+                print(guess)
+
+                if event.key == pygame.K_RETURN and win == True:
+                    main()
+
+                if event.key == pygame.K_RETURN and turns == 6:
+                    main()
+
+                if event.key == pygame.K_RETURN and len(guess) > 4 :
+                    print('checking')
+                    if guess in GUESS_WORDS:
+                        win = checkGuest(turns, word, guess, window)
+                        turns +=1 
+                        guess = ""
+                        window.fill(BLACK, (0, 500, 500, 200))
+                    else:
+                        window.blit(notValid, (180, 530))
+
+        window.fill(BLACK, (0, 500, 500, 200))
+        renderGuess = font.render(guess, True, WHITE)
+        window.blit(renderGuess, (180, 530))
+
+        if win == True:
+            window.blit(youWin, (175,200))
+            window.blit(playAgain, (120, 300))
+
+        if turns == 6 and win != True:
+            window.blit(youLose, (175,200))
+            window.blit(playAgain, (120, 300))
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+main()
